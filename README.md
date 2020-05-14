d6cee8e8c8f7cc56deb91f09e438e04d3cd2133a
alezi06 <alezi06@mail.ru>
2020-05-14 22:36:53 +0300

    step3

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ How to install:
 ```
 python -m pip install --user -i https://test.pypi.org/simple/ alezi06-gendiff --extra-index-url https://pypi.org/simple/
 ```
+
+[![asciicast](https://asciinema.org/a/bHxLgzEHcXUIzJYnfMkvqSBLn.svg)](https://asciinema.org/a/bHxLgzEHcXUIzJYnfMkvqSBLn)
---
+++ gendiff/generate_diff.py
@@ -0,0 +1,30 @@
+import json
+
+
+def to_str(key, value, flag=' '):
+    return f'  {flag} {key}: {value}'
+
+
+def generate_diff(path_to_file1, path_to_file2):
+    before = json.load(open(path_to_file1))
+    after = json.load(open(path_to_file2))
+
+    before_keys = set(before)
+    after_keys = set(after)
+    
+    result = []
+    for key in before_keys & after_keys:
+        if before[key] == after[key]:
+            result.append(to_str(key, after[key]))
+        else:
+            result.append(to_str(key, after[key], '+'))
+            result.append(to_str(key, before[key], '-'))
+    
+    result.extend(
+        [to_str(key, before[key], '-') for key in before_keys - after_keys]
+    )
+    result.extend(
+        [to_str(key, after[key], '+') for key in after_keys - before_keys]
+    )
+
+    return '{\n' + '\n'.join(result) + '\n}'
